a63bbc01b5e43fa60fcf17146355e79d100866bb
Emiliano Fant <emiliano.fant@split.io>
2021-04-16 12:22:03 -0300

    test: fixed test ts

--- src/__tests__/utils/mockBrowserSplitSdk.ts
+++ src/__tests__/utils/mockBrowserSplitSdk.ts
@@ -62,7 +62,7 @@ export function mockSdk() {
       });
       const ready: jest.Mock = jest.fn(() => {
         return promiseWrapper(new Promise((res, rej) => {
-          __isReady__ ? res() : __emitter__.on(Event.SDK_READY, res);
+          __isReady__ ? res(null) : __emitter__.on(Event.SDK_READY, res);
           __hasTimedout__ ? rej() : __emitter__.on(Event.SDK_READY_TIMED_OUT, rej);
         }), () => { });
       });
