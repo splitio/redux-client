3f8650a1d6ebc74459b05975bba9ab1b87384a11
Emiliano Sanchez <emiliano.sanchez@split.io>
2023-03-28 19:42:17 +0200

    polishing

--- jest.config.js
+++ jest.config.js
@@ -10,11 +10,11 @@ module.exports = {
   },
 
   // Test files are .js, .jsx, .ts and .tsx files inside of __tests__ folders and with a suffix of .test or .spec
-  testMatch: [ "**/__tests__/**/?(*.)+(spec|test).[jt]s?(x)" ],
+  testMatch: [ '**/__tests__/**/?(*.)+(spec|test).[jt]s?(x)' ],
 
   // Included files for test coverage (npm run test:coverage)
   collectCoverageFrom: [
-    "src/**/*.{js,jsx,ts,tsx}",
-    "!src/__tests__/**",
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/__tests__/**',
   ]
 };
